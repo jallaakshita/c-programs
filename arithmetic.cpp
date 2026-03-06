d251656132f276a720900c02e872df0df4e129bb
jallaakshita <jallaakshita24@gmail.com>
2026-03-06 21:22:46 +0530

    Add files via upload

---
+++ arithmetic.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e;
+	printf("enter two numbers");
+	scanf("%d%d",&a,&b);
+	c=a*b;
+	d=a+b;
+	e=a-b;
+	printf("values %d",c);
+	printf("values %d",d);
+	printf("values %d",e);
+	printf("values %d",d);
+}
