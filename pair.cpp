dd056de2c39854144756c3dbcbdff36bc7d53588
jallaakshita <jallaakshita24@gmail.com>
2026-03-29 17:33:02 +0530

    Add files via upload

---
+++ pair.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	pair<int,string>p,p1;
+	p={91,"akki"};
+	cout<<p.first<<endl;
+	cout<<p.second<<endl;
+	cin>>p1.first;
+	cin>>p1.second;
+	cout<<p1.first<<endl<<p1.second;
+}
